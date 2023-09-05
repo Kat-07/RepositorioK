f9d1a0ad8cdbc38eaa68c4ae41dbe6e24b4b3ec4
303 - Programacion <303@laboratorios.usbbog.edu.co>
2023-09-05 13:20:34 -0500

    Actualizacion

---
+++ CORTE1/Parcial.py/Ext.py
@@ -0,0 +1,24 @@
+import random as r
+
+def Repartir():
+    respuesta=input('¿Desea pedir una carta? ingrese yes')
+    if respuesta==yes:
+        z=r.randing(1,9)
+        print(x,y,z)
+
+    Cartas=[A,1,2,3,4,5,6,7,8,9,J,Q,K]
+    A=11 or 1; J=10; Q=10; K=10
+    x=r.randing(1,9)
+    y=r.choice(A,K)
+    print('Cartas:',x,y)
+    X=x,y
+    print('Conteo', X)
+   
+
+if __name__=="__main__":
+    Repartir()
+
+
+    
+
+
---
+++ CORTE1/Parcial.py/Principalparcial.py
@@ -0,0 +1,16 @@
+import random as r
+
+def main(): #se define una funcion 
+
+        print('Bienvenido a Blackjack')
+        print('Que comience el juego')
+
+def repartir(:)
+        cartas=[A,1,2,3,4,5,6,7,8,9,J,Q,K]
+        A=1 or 11;J=10;Q=10;K=10
+
+
+if __name__=="__main__":
+    main()
+    repartir()
+     
